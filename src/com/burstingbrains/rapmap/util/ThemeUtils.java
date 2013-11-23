888a2ac1c7d044e1ad0ab192af04a625230a25cd
Tom Yu <tom.yu@burstingbrains.com>
2013-11-23 03:01:23 -0600

    added themes sadcrow, dreammagnet, circlingsun, antiquebird, midnight, romantic

--- src/com/burstingbrains/rapmap/util/ThemeUtils.java
+++ src/com/burstingbrains/rapmap/util/ThemeUtils.java
@@ -9,7 +9,11 @@ public class ThemeUtils {
 
 	public enum Theme{ 
 		THEME_SADCROW,
-		THEME_DREAMMAGNET;
+		THEME_DREAMMAGNET,
+		THEME_CIRCLINGSUN,
+		THEME_ANTIQUEBIRD,
+		THEME_MIDNIGHT,
+		THEME_ROMANTIC;
 
 		public Theme getNext() {
 			if(this.ordinal() < Theme.values().length - 1){
@@ -36,6 +40,18 @@ public class ThemeUtils {
 			break;
 		case THEME_DREAMMAGNET:
 			activity.setTheme(R.style.theme_dreammagnet);
+			break;
+		case THEME_CIRCLINGSUN:
+			activity.setTheme(R.style.theme_circlingsun);
+			break;
+		case THEME_ANTIQUEBIRD:
+			activity.setTheme(R.style.theme_antiquebird);
+			break;
+		case THEME_MIDNIGHT:
+			activity.setTheme(R.style.theme_midnight);
+			break;
+		case THEME_ROMANTIC:
+			activity.setTheme(R.style.theme_romantic);
 			break;
 		default:
 			break;
